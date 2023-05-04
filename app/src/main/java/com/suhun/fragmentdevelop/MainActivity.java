9683bac9370e63714c93303b851cbaccfa05db13
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-04 09:15:55 +0800

    Init Fragment

--- app/src/main/java/com/suhun/fragmentdevelop/MainActivity.java
+++ app/src/main/java/com/suhun/fragmentdevelop/MainActivity.java
@@ -1,16 +1,30 @@
 package com.suhun.fragmentdevelop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private F1Fragment f1;
+    private F2Fragment f2;
+    private F3Fragment f3;
+    private FragmentManager fmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        f1 = new F1Fragment();
+        f2 = new F2Fragment();
+        f3 = new F3Fragment();
+        fmgr = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.add(R.id.container, f1);
+        transaction.commit();
     }
 
     public void goToPage1(View view){
