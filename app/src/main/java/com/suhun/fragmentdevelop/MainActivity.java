5c9a1af9dd044a9cb6ba358b500b2e5aa471e838
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-04 09:02:44 +0800

    Add Main Page layout

--- app/src/main/java/com/suhun/fragmentdevelop/MainActivity.java
+++ app/src/main/java/com/suhun/fragmentdevelop/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.fragmentdevelop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToPage1(View view){
+
+    }
+
+    public void goToPage2(View view){
+
+    }
+
+    public void goToPage3(View view){
+
+    }
+
 }
