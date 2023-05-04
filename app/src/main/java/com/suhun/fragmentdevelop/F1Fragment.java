1bd4c6e4e32e159f2af12a0137512298c322d285
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-04 09:36:44 +0800

    Add Button and TextView to Fragment and implement code in F1Fragment.java not MainActivity.java

--- app/src/main/java/com/suhun/fragmentdevelop/F1Fragment.java
+++ app/src/main/java/com/suhun/fragmentdevelop/F1Fragment.java
@@ -7,15 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class F1Fragment extends Fragment {
     private  View view;
+    private Button createBtn;
+    private TextView lotteryShow;
+    private int lottery;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if(view == null) {
             // Inflate the layout for this fragment
             view = inflater.inflate(R.layout.fragment_f1, container, false);
+            createBtn = view.findViewById(R.id.create);
+            lotteryShow = view.findViewById(R.id.lotteryNum);
+            createBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    lottery = new Random().nextInt(49)+1;
+                    lotteryShow.setText(""+lottery);
+                }
+            });
         }
         return view;
     }
