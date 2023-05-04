29400a2dfbc47534a405f427ca4ed30687485f2d
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-04 09:24:26 +0800

    Move view to property

--- app/src/main/java/com/suhun/fragmentdevelop/F1Fragment.java
+++ app/src/main/java/com/suhun/fragmentdevelop/F1Fragment.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class F1Fragment extends Fragment {
+    private  View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        if(view == null) {
+            // Inflate the layout for this fragment
+            view = inflater.inflate(R.layout.fragment_f1, container, false);
+        }
+        return view;
     }
 }
