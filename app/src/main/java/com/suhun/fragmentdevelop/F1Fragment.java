3494ab53a99bd1629c694ee6b2ecb08f0ba60ef6
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-04 09:09:43 +0800

    Add 3 Fragment and layout to project

---
+++ app/src/main/java/com/suhun/fragmentdevelop/F1Fragment.java
@@ -0,0 +1,18 @@
+package com.suhun.fragmentdevelop;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class F1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_f1, container, false);
+    }
+}
